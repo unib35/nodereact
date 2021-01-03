ea0ece0e0c352f46e2144e0d32a0b987d53be152
Lee <unib335@naver.com>
2021-01-03 00:51:02 +0000

    Sign Up applicaiton

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const { User } = require("./models/Users");
+const bodyParser = require('body-parser');
 
-
+//application/x--www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://unib35:whdalsdl0178@nodereact.sddit.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -12,9 +17,26 @@ mongoose.connect('mongodb+srv://unib35:whdalsdl0178@nodereact.sddit.mongodb.net/
 .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => {
-  res.send('서버 상태 : 양호')
+app.get('/', (req, res) => {res.send('서버 상태 : 양호')})
+
+app.post('/register', (req, res) => {
+	
+	//회원가입 할 때 필요한 정보들을 client에서 가져오면
+	//그것들을 datebase에 넣어준다.
+	
+	const user = new User(req.body)
+	
+	user.save((err, userInfo) => {
+		if(err) return res.json({ success: false, err})
+		return res.status(200).json({
+			success: true
+		})
+	})
+	
+	
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at https://nodereact-lee.run.goorm.io in port: ${port}`)
